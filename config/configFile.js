e0cb7740b9a64f4ba6a168d3b22b22c1691b56a0
Rhynz <42452952+RhynzID@users.noreply.github.com>
2023-01-08 14:43:23 +0700

    Add files via upload

---
+++ config/configFile.js
@@ -0,0 +1,14 @@
+exports.info = {
+    prefix: '.',
+      prefix2: '#',
+      owner: {
+          nome: "Rhynz",
+          number: "6282261637676",
+          numero: ["6282261637676"]
+      }, 
+      bot: {
+          name: "rhynz bot",
+          number: "62895400244287"
+      },
+      grupo: ""
+  }
